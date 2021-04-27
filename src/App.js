0efde8b4845cf0d576d8b7418310bbd9ef0d2e21
adityajain-web <60445026+adityajain-web@users.noreply.github.com>
2021-04-27 14:49:00 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Heading from './Component/Heading';
+import Netflix from './Component/Netflix';
+import Amazon from './Component/Amazon';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+let favOtt = prompt("Your favourite ott platform: Netflix or Amazon").toUpperCase();
+
+const App = () => {
+  return (<>
+    <Heading ott={favOtt} />
+    <div id="seriesContainer">
+      {(favOtt === "NETFLIX") ? <Netflix /> : <Amazon />}
     </div>
-  );
+  </>)
 }
 
 export default App;
---
+++ src/SeriesData.jsx
@@ -0,0 +1,38 @@
+const series = [{
+    id: 1,
+    sName: "DARK",
+    imgSrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80990668?source=35",
+    ott: "Netflix"
+}, {
+    id: 2,
+    sName: "Extra Curricular",
+    imgSrc: "https://www.hitc.com/static/uploads/hitcn/1816/extracurricular_netflix_780_1544944.jpg",
+    title: "Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80990668?source=35",
+    ott: "Netflix"
+}, {
+    id: 3,
+    sName: "Stranger Things",
+    imgSrc: "https://wallpapercave.com/wp/wp1917154.jpg",
+    title: "Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80990668?source=35",
+    ott: "Netflix"
+}, {
+    id: 4,
+    sName: "The Vampire Diary",
+    imgSrc: "https://images.justwatch.com/poster/8589251/s592",
+    title: "Amazon Prime Series",
+    link: "https://www.netflix.com/in/title/80990668?source=35",
+    ott: "Amazon"
+}, {
+    id: 5,
+    sName: "My First First Love",
+    imgSrc: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+    title: "Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80990668?source=35",
+    ott: "Netflix"
+}];
+
+export default series;
